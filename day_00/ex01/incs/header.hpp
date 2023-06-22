9b601fc6b94a48da3910b59ed7033e38098e2019
yrhiba <yahyarhiba@gmail.com>
2023-06-22 23:57:05 +0100

    Touch Files needed class, incs.

---
+++ day_00/ex01/incs/header.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/22 23:39:14 by yrhiba            #+#    #+#             */
+/*   Updated: 2023/06/22 23:40:21 by yrhiba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER
+# define HEADER
+
+#include <iostream>
+#include <string>
+#include "Contact.hpp"
+#include "PhoneBook.hpp"
+
+#endif
