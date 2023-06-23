eed74fbd2f52ecd110e42cb89bbd487fdbd7acc5
yrhiba <yahyarhiba@gmail.com>
2023-06-23 01:54:08 +0100

    adding getline for reading the command line

--- day_00/ex01/main.cpp
+++ day_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 23:37:52 by yrhiba            #+#    #+#             */
-/*   Updated: 2023/06/22 23:40:38 by yrhiba           ###   ########.fr       */
+/*   Updated: 2023/06/23 01:53:00 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 int main()
 {
+	PhoneBook phonebook;
+
+	while (1)
+	{
+		std::string s;
+		std::getline(std::cin, s);
+
+		// if (s == "EXIT")
+		// 	return (0);
+		// else if (s == "ADD")
+		// 	phonebook.add();
+		// else if (s == "SEARCH")
+		// 	phonebook.shearch();
+		// else
+		// 	std::cout << "command not found\n";
+
+		std::cout << s << std::endl;
+	}
 	return (0);
 }
