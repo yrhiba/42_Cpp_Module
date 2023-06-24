511295e7276cb1c48a4e56ca76f9f1105d159c03
yrhiba <yahyarhiba@gmail.com>
2023-06-24 13:36:02 +0100

    still working on PhoneBook + fixing Makefile

--- day_00/ex01/main.cpp
+++ day_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 23:37:52 by yrhiba            #+#    #+#             */
-/*   Updated: 2023/06/23 02:24:17 by yrhiba           ###   ########.fr       */
+/*   Updated: 2023/06/23 02:50:00 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,27 @@ int main()
 	while (1)
 	{
 		std::string s;
-		std::getline(std::cin, s);
+		if (!std::getline(std::cin, s))
+			return (0);
+		std::cout << "Entred line : " << s << std::endl;
 
 		if (s == "EXIT")
 			return (0);
 		else if (s == "ADD")
+		{
 			if (!phonebook.add())
 			{
 				std::cout << "Erorr:: All fields must be no empty!\n";
 				std::cout << "Contact was not add to The Phonebook\n";
 				std::cout << "Please readd whit a valid values\n";
 			}
+		}
 		else if (s == "SEARCH")
 			phonebook.shearch();
+		else if (s.empty())
+			continue;
 		else
 			std::cout << "command not found\n";
-
-		std::cout << s << std::endl;
 	}
 	return (0);
 }
