0588a1cd06169587a83833c15cb12098e4c86df6
yrhiba <yahyarhiba@gmail.com>
2023-06-25 03:40:52 +0100

    creating day01-ex02 and initializ files

---
+++ day_01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 03:36:18 by yrhiba            #+#    #+#             */
+/*   Updated: 2023/06/25 03:40:01 by yrhiba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string string		= "HI THIS IS BRAIN";
+	std::string *stringPtr	= &string;
+	std::string &stringRef	= string;
+
+	std::cout << "Adrr Of string: " << &string << std::endl;
+	std::cout << "Value Of stringPtr: " << stringPtr << std::endl;
+	std::cout << "Adrr Of stringRef: " << &stringRef << std::endl;
+
+
+	return (0);
+}
