1db48a00bc234969b4da70bdba1edd9ba156bf9e
yrhiba <yahyarhiba@gmail.com>
2023-08-28 13:59:49 +0100

    Adding ex00-day04.
    this is the last day of the first part
    in the pool of cpp at 1337 coding school.
    it's about OOP and polymorphisme.

---
+++ day_04/ex00/header.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrhiba <yrhiba@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 13:21:55 by yrhiba            #+#    #+#             */
+/*   Updated: 2023/08/28 13:40:20 by yrhiba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _HEADER
+# define _HEADER
+
+/* standard includes */
+#include <iostream>
+#include <string>
+
+/* relative includes */
+#include "Animal.hpp"
+
+
+#endif
