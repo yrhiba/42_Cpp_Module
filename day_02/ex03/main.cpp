41d618af370a2ed3011d2b954ddaccd4fd34118c
yrhiba <yahyarhiba@gmail.com>
2023-09-05 18:13:21 +0100

    removing using namespace std;
    ex03-day02

--- day_02/ex03/main.cpp
+++ day_02/ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yrhiba <yrhiba@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 00:21:27 by yrhiba            #+#    #+#             */
-/*   Updated: 2023/08/21 06:36:40 by yrhiba           ###   ########.fr       */
+/*   Updated: 2023/09/05 18:12:22 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 #include "Point.hpp"
 
 bool bsp( Point const a, Point const b, Point const c, Point const point);
-
-using namespace std;
 
 int main( void )
 {
