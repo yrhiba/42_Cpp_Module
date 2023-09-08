e4ef8dee8f52b145b4ae5677f3339b027ca0d54e
yrhiba <yahyarhiba@gmail.com>
2023-09-08 19:58:25 +0100

    adding interface for Character class

---
+++ day_04/ex03/ICharacter.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 14:18:37 by yrhiba            #+#    #+#             */
+/*   Updated: 2023/09/08 17:56:17 by yrhiba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _ICHARCH
+# define _ICHARCH
+
+# include "header.hpp"
+
+class ICharacter
+{
+public:
+	virtual ~ICharacter() {}
+	virtual std::string const & getName() const = 0;
+	virtual void equip(AMateria* m) = 0;
+	virtual void unequip(int idx) = 0;
+	virtual void use(int idx, ICharacter& target) = 0;
+};
+
+#endif
