4af810a5e0cfc67b6fc49e083ecd398670b762a6
yrhiba <yahyarhiba@gmail.com>
2024-03-07 16:33:41 +0100

    modified:   day_05/ex01/main.cpp

--- day_05/ex01/main.cpp
+++ day_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 15:27:15 by yrhiba            #+#    #+#             */
-/*   Updated: 2024/03/06 22:38:49 by yrhiba           ###   ########.fr       */
+/*   Updated: 2024/03/07 16:31:35 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int main()
 {
-	Bureaucrat a("A-Beraucrat", 10);
+	Bureaucrat a("A-Beraucrat", 23);
 	Form f("X-form", 10, 5);
 	std::cout << f << std::endl;
 	// f.beSigned(a);
