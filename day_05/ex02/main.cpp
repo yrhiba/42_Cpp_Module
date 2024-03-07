b9696d7453fb906577d550e98aeb823e0ef820da
yrhiba <yahyarhiba@gmail.com>
2024-03-07 21:40:05 +0100

    modified:   day_05/ex02/main.cpp

--- day_05/ex02/main.cpp
+++ day_05/ex02/main.cpp
@@ -6,16 +6,25 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 15:27:15 by yrhiba            #+#    #+#             */
-/*   Updated: 2024/03/07 16:58:25 by yrhiba           ###   ########.fr       */
+/*   Updated: 2024/03/07 21:39:19 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 int main()
 {
-	Bureaucrat a("A-Beraucrat", 10);
-	// AForm *form = AForm("name", 150, 150);
+	Bureaucrat a("A-Beraucrat", 1);
+
+	AForm *form = new ShrubberyCreationForm("yahya");
+	form->beSigned(a);
+
+	std::cout << a << std::endl;
+	std::cout << *form << std::endl;
+
+	delete form;
+
 	return (0);
 }
