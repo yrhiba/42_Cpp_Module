662b5b01c090db161621610c84bade28c975a1a2
yrhiba <yahyarhiba@gmail.com>
2024-03-12 01:06:32 +0000

    modified:   day_07/ex01/iter.hpp 	modified:   day_07/ex01/main.cpp

--- day_07/ex01/iter.hpp
+++ day_07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 19:09:31 by yrhiba            #+#    #+#             */
-/*   Updated: 2024/03/11 20:20:08 by yrhiba           ###   ########.fr       */
+/*   Updated: 2024/03/12 01:06:14 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,6 @@ template<typename T, typename F>
 void iter(T *arr, size_t _size, F func)
 {
 	for (size_t i = 0; i < _size; i++) func(*(arr + i));
-}
-
-template<typename T>
-void print(const T & e)
-{
-	std::cout << e << std::endl;
 }
 
 #endif
--- day_07/ex01/main.cpp
+++ day_07/ex01/main.cpp
@@ -6,12 +6,18 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 19:08:53 by yrhiba            #+#    #+#             */
-/*   Updated: 2024/03/11 20:20:07 by yrhiba           ###   ########.fr       */
+/*   Updated: 2024/03/12 01:06:14 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
 # include "iter.hpp"
+
+template<typename T>
+void print(const T & e)
+{
+	std::cout << e << std::endl;
+}
 
 int main()
 {
