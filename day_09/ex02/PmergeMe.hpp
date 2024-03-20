16d5ed290c4cb0b693f428999b3cb6c172a7b1ca
yrhiba <yahyarhiba@gmail.com>
2024-03-20 18:05:38 +0000

    new file:   day_09/ex02/Makefile 	modified:   day_09/ex02/PmergeMe.cpp 	modified:   day_09/ex02/PmergeMe.hpp 	modified:   day_09/ex02/main.cpp 	deleted:    day_09/ex02/merge_sort.cpp

--- day_09/ex02/PmergeMe.hpp
+++ day_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 10:54:49 by yrhiba            #+#    #+#             */
-/*   Updated: 2024/03/18 10:59:15 by yrhiba           ###   ########.fr       */
+/*   Updated: 2024/03/20 18:02:23 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,113 @@
 # include <algorithm>
 # include <utility>
 # include <iomanip>
+# include <list>
+# include <deque>
+# include <limits>
+# include <sys/time.h>
+# include <ctime>
+# include <unistd.h>
 
 class PmergeMe
 {
 private:
-
-
-public:
+	PmergeMe() {};
+	//
 	template<typename T>
-	static void sort(T &c)
+	static void merge_sort(T &a)
 	{
-		typedef typename T::iterator iterator;
-
-		for (iterator it = c.begin(); it != c.end(); it++)
+		if (a.size() <= 2)
 		{
-			std:cout << *it << " ";
+			if (a.size() == 2 && *a.begin() > *a.rbegin()) swap(*a.begin(), *a.rbegin());
+			return ;
 		}
 		//
-		std::cout << std::endl;
+		T half;
+		//
+		for (size_t i = 0; i < a.size() / 2; i++)
+		{
+			half.push_back(a.back());
+			a.pop_back();
+		}
+		//
+		merge_sort(a);
+		merge_sort(half);
+		//
+		T res;
+		merge(a.begin(), a.end(), half.begin(), half.end(), std::back_inserter(res));
+		//
+		a = res;
+		return ;
+	}
+
+	template<typename T>
+	static T jacobSeq(int n)
+	{
+		T a;
+		int i1 = 1;
+		int i2 = 1;
+		while (i2 < n)
+		{
+			int cur = i1 * 2 + i2;
+			for (int i = std::min(n, cur); i > i2; i--) a.push_back(i - 1);
+			i1 = i2;
+			i2 = cur;
+		}
+		return a;
+	}
+
+public:
+	template<typename T, typename C>
+	static void sort(T &c)
+	{
+		typedef typename T::iterator it;
+		typedef typename C::iterator cit;
+		//
+		C a;
+		//
+		while (c.size() > 1)
+		{
+			int i = c.back(); c.pop_back();
+			int j = c.back(); c.pop_back();
+			//
+			int mmax = std::max(i, j);
+			int mmin = std::min(i, j);
+			//
+			a.push_back(std::make_pair(mmax, mmin));
+		}
+		//
+		PmergeMe::merge_sort(a);
+		//
+		T s; // final sorted elements
+		//
+		for (cit i = a.begin(); i != a.end(); i++)
+		{
+			if (i == a.begin()) s.push_back(i->second);
+			//
+			s.push_back(i->first);
+		}
+		//
+		T jacob = PmergeMe::jacobSeq<T>(a.size());
+		//
+		for (it i = jacob.begin(); i != jacob.end(); i++)
+		{
+			cit j = a.begin(); // looking for the current element to insert
+			std::advance(j, *i);
+			//
+			it end = std::lower_bound(s.begin(), s.end(), j->first);
+			//
+			it t = std::lower_bound(s.begin(), end, j->second);
+			s.insert(t, j->second);
+		}
+		//
+		if (!c.empty())
+		{
+			it t = std::lower_bound(s.begin(), s.end(), c.back());
+			s.insert(t, c.back());
+		}
+		//
+		c = s;
 	}
 };
-
-
 
 #endif
--- day_09/ex02/main.cpp
+++ day_09/ex02/main.cpp
@@ -6,13 +6,75 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 10:53:56 by yrhiba            #+#    #+#             */
-/*   Updated: 2024/03/18 10:54:13 by yrhiba           ###   ########.fr       */
+/*   Updated: 2024/03/20 18:02:43 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "PmergeMe.hpp"
+
+void err(std::string msg)
+{
+	std::cout << msg << std::endl;
+	exit(0);
+}
+
+long long counttime(struct timeval &start, struct timeval &end)
+{
+	long long seconds = end.tv_sec - start.tv_sec;
+	long long microseconds = end.tv_usec - start.tv_usec;
+	long long duration = (seconds * 1e6) + microseconds;
+	//
+	return (duration);
+}
 
 int main(int ac, char **av)
 {
-
+	std::list<int> list;
+	std::deque<int> deque;
+	// Parsing Part
+	for (int i = 1; i < ac; i++)
+	{
+		long long value = 0;
+		//
+		for (int j = 0; av[i][j]; j++)
+		{
+			if (!(std::isdigit(av[i][j]))) err(" error bad arguments");
+			//
+			value = (value * 10) + (av[i][j] - '0');
+			//
+			if (value > std::numeric_limits<int>::max()) err("error number too long");
+		}
+		//
+		list.push_back(value);
+		deque.push_back(value);
+	}
+	//
+	std::cout << "Before: ";
+	for (std::list<int>::iterator it = list.begin(); it != list.end(); it++) std::cout << *it << " ";
+	std::cout << std::endl;
+	// time vars
+	struct timeval start_list, end_list, start_deque, end_deque;
+	// sort list container
+	gettimeofday(&start_list, NULL);
+	PmergeMe::sort<std::list<int>, std::list< std::pair<int, int> > >(list);
+	gettimeofday(&end_list, NULL);
+	// sort deque container
+	gettimeofday(&start_deque, NULL);
+	PmergeMe::sort<std::deque<int>, std::deque< std::pair<int, int> > >(deque);
+	gettimeofday(&end_deque, NULL);
+	//
+	std::cout << "After: ";
+	for (std::list<int>::iterator it = list.begin(); it != list.end(); it++) std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "After: ";
+	for (std::deque<int>::iterator it = deque.begin(); it != deque.end(); it++) std::cout << *it << " ";
+	std::cout << std::endl;
+	//
+	std::cout << "Time to process a range of " << list.size() << " elements with std::list : ";
+	std::cout << counttime(start_list, end_list) << " us" << std::endl;
+	//
+	std::cout << "Time to process a range of " << deque.size() << " elements with std::deque : ";
+	std::cout << counttime(start_deque, end_deque) << " us" << std::endl;
+	//
 	return (0);
 }
